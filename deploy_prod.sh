abb998ea089ca935c87ff0a33c35415b5620f2d0
Yuya Kakui <mail@kakuiyuya.com>
2016-01-10 23:21:28 +0900

    up

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd web
-heroku run bundle exec rake db:migrate
+heroku run bundle exec rake db:migrate --app coneco-cat-web
 git push git@heroku.com:coneco-cat-web.git $CIRCLE_SHA1:refs/heads/master
